cbf8f72a5f8e44ca8d6245923ac816fabb8be48b
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 04:58:21 +0000

    Auto-format by https://ultralytics.com/actions

--- train.py
+++ train.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-    model = YOLO('yolov8m.pt')
+if __name__ == "__main__":
+    model = YOLO("yolov8m.pt")
     model.train(
-        data='dataset/data.yaml',
+        data="dataset/data.yaml",
         epochs=50,
         imgsz=640,
         batch=16,
         device=0,
-        project='runs/train',
-        name='waste-exp1',
-        exist_ok=True
+        project="runs/train",
+        name="waste-exp1",
+        exist_ok=True,
     )
